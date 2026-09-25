80ea149fe7b4c065a83d2b64706021ca10d5f544
josie <160166451+josiefinis@users.noreply.github.com>
2026-09-25 17:41:04 +0200

    Revert "feat: demonstrate basic database functionality"
    This reverts commit 03c10e8b69868cc4fa859ad033321cd29ca56310.

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,20 @@
+
 import Link from "next/link";
-import { getProducts } from "./lib/database";
 
-export default async function HomePage() {
-  const products = await getProducts({ limit: 8 });
 
-  return (
-    <div>
-      <h1 className="text-3xl"> Webshop Home page </h1>
+export default function HomePage()
+{
+    return(
+        <div>
+            <h1 className="text-3xl"> Webshop Home page </h1>
 
-      {/* Temporary links below */}
-      <br />
-      <Link href="/product/id" className="underline">
-        Go to product details page
-      </Link>
-      <br />
-      <Link href="/cart" className="underline">
-        Go to Cart page
-      </Link>
-      <br />
-      <Link href="/admin" className="underline">
-        Go to Admin page
-      </Link>
-
-      {products.map((product) => (
-        <p
-          key={product.id}
-        >{`${product.title} ${product.brand} ${product.price} ${product.description}`}</p>
-      ))}
-    </div>
-  );
+            {/* Temporary links below */}
+            <br/>
+            <Link href="/product/id" className="underline"> Go to product details page </Link>
+            <br/>
+            <Link href="/cart" className="underline"> Go to Cart page </Link>
+            <br/>
+            <Link href="/admin" className="underline"> Go to Admin page </Link>
+        </div>
+    );
 }
